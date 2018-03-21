6e2f3c3f80bdd8becce7158ef74348f4d24875ae
liuhaitao <liuhaitao@niwodai.net>
2018-03-21 19:08:19 +0800

    提交

--- src/test/java/com/tianhui/zhishu/MemcachedTestCase/MemcachedTestCase.java
+++ src/test/java/com/tianhui/zhishu/MemcachedTestCase/MemcachedTestCase.java
@@ -1,31 +1,31 @@
-package com.tianhui.zhishu.MemcachedTestCase;
-
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.common.utils.memcached.MemcachedClientUtils;
-import com.common.utils.memcached.NoSQLService;
-import com.tianhui.zhishu.SpringBootTestCaseBase;
-
-/**
- *
- * Memcached测试.
- *
- * @author:haitao.liu
- *
- */
-public class MemcachedTestCase extends SpringBootTestCaseBase {
-
-    @Autowired
-    private MemcachedClientUtils memcachedClientUtils;
-
-    @Autowired
-    private NoSQLService noSQLService;
-
-    @Test
-    public void testJsonUtil() {
-        noSQLService.set("aaa", "bbbb");
-        System.err.println(noSQLService.get("aaa") + "---------------");
-
-    }
-}
+//package com.tianhui.zhishu.MemcachedTestCase;
+//
+//import org.junit.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import com.common.utils.memcached.MemcachedClientUtils;
+//import com.common.utils.memcached.NoSQLService;
+//import com.tianhui.zhishu.SpringBootTestCaseBase;
+//
+///**
+// *
+// * Memcached测试.
+// *
+// * @author:haitao.liu
+// *
+// */
+//public class MemcachedTestCase extends SpringBootTestCaseBase {
+//
+//    @Autowired
+//    private MemcachedClientUtils memcachedClientUtils;
+//
+//    @Autowired
+//    private NoSQLService noSQLService;
+//
+//    @Test
+//    public void testJsonUtil() {
+//        noSQLService.set("aaa", "bbbb");
+//        System.err.println(noSQLService.get("aaa") + "---------------");
+//
+//    }
+//}
